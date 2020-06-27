5620070eb9901545f93b6151da887fd1323481ba
Mikel1991 <mikel362d@gmail.com>
2020-06-27 15:55:40 -0700

    file orm changed to ES6

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,82 @@
 // Import MySQL connection.
 const connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+      arr.push("?");
+    }
+    return arr.toString();
+  }
+  
+  // Helper function to convert object key/value pairs to SQL syntax
+  function objToSql(ob) {
+    let arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (let key in ob) {
+      let value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations 
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  }
+  
+  // Object for all our SQL statement functions.
+  const orm = {
+    all: (tableInput, cb) => {
+      const queryString = `SELECT * FROM ${tableInput};`;
+      connection.query(queryString, (err, result) => {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    create: (table, cols, vals, cb) => {
+      const queryString = `INSERT INTO ${table} (${cols.toString()}) VALUES (${printQuestionMarks(vals.length)})`
 
+      console.log(queryString);
+  
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    // An example of objColVals would be {name: panther, sleepy: true}
+    update: (table, objColVals, condition, cb) => {
+      const queryString = `UPDATE ${table} SET ${objToSql(objColVals)} WHERE ${condition}`;
+
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    delete: (table, condition, cb) => {
+      const queryString = `DELETE FROM ${table} WHERE ${condition}`;
+
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    }
+  };
 
 // Export the orm object for the model (cat.js).
 module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-// const burger = require("../models/burger.js");
+const burger = require("../models/burger.js");
 
 
 
