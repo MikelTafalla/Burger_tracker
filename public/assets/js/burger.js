6e68b8eb92dee39e19ee84d4f129526ca94ac110
Mikel1991 <mikel362d@gmail.com>
2020-06-27 18:22:13 -0700

    devoured button added to burgers list

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,25 +1,23 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    // $(".change-sleep").on("click", function(event) {
-    //   var id = $(this).data("id");
-    //   var newSleep = $(this).data("newsleep");
+    $(".change-devoured").on("click", event => {
+      let id = $(this).data("id");
+      let newDevoured = {
+        devoured: 1
+      };
   
-    //   var newSleepState = {
-    //     sleepy: newSleep
-    //   };
-  
-    //   // Send the PUT request.
-    //   $.ajax("/api/burgers/" + id, {
-    //     type: "PUT",
-    //     data: newSleepState
-    //   }).then(
-    //     function() {
-    //       console.log("changed sleep to", newSleep);
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevoured
+      }).then(
+        function() {
+          console.log("changed devoured to", newDevoured);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
   
     $(".create-form").on("submit", event => {
       // Make sure to preventDefault on a submit event.
