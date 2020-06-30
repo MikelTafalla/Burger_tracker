8f15bc6803a05447132e78697f8e7818303b2ab2
Mikel1991 <mikel362d@gmail.com>
2020-06-30 12:54:59 -0700

    connection.js corrected

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,20 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
-const connection;
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "icopoghru9oezxh8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "xsv6rj27xgnnqv8b",
-    password: "yxw6rxwosye8zqlq",
-    database: "fooakocmv2qlgkeu"
-  });
-};
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "R.sociedad789",
+    database: "burgers_db"
+  })
+}
 
 // Make connection.
-connection.connect(err => {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
