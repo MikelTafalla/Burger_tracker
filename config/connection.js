95fa131db9ef7c27d10e8c8e0ae6937b0e4f40cc
Mikel1991 <mikel362d@gmail.com>
2020-06-30 12:13:30 -0700

    connecton changed for heroku deployement

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+const connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "R.sociedad789",
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "R.sociedad789",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(err => {
